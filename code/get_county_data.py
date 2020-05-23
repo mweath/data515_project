ec32b7522e1fca31f67635ebdac7196fab725f51
chrico7 <50036711+chrico7@users.noreply.github.com>
2020-05-23 12:36:16 -0700

    Added new file_name logic

--- code/get_county_data.py
+++ code/get_county_data.py
@@ -6,6 +6,7 @@ def get_county_data(file_name, num_rows=None):
 
     Args:
         file_name: The name of the file to download.
+                   Spaces will be properly formatted
         num_rows: The number of rows to return.
 
     Returns:
@@ -15,6 +16,7 @@ def get_county_data(file_name, num_rows=None):
 
     Raises:
         ValueError: If passed file_name is not a string.
+        ValueError: If passed file_name is not valid.
         ValueError: If passed num_rows is not a positive integer.
         OSError: If a connection to the URL is unable to be established.
     """
@@ -27,8 +29,29 @@ def get_county_data(file_name, num_rows=None):
     data_raw = pd.DataFrame()
 
     # Check inputs
+    valid_names = ['Accessory', 'Apartment%20Complex', 'Change%20History',
+                   'Change%20History%20Detail', 'Commercial%20Building',
+                   'Condo%20Complex%20and%20Units',
+                   'District%20Levy%20Reference',
+                   'Environmental%20Restriction',
+                   'Home%20Improvement%20Applications',
+                   'Home%20Improvement%20Exemptions', 'Legal', 'Lookup',
+                   'Notes', 'Parcel', 'Permit', 'Real%20Property%20Account',
+                   'Real%20Property%20Appraisal%20History',
+                   'Real%20Property%20Sales', 'Residential%20Building',
+                   'Review%20History', 'Tax%20Data', 'Unit%20Breakdown',
+                   'Vacant%20Lot', 'Value%20History']
+
     if not isinstance(file_name, str):
         raise ValueError('Passed file_name must be of type string')
+
+    file_name = file_name.replace(' ', '%20')
+
+    if file_name not in valid_names:
+        raise ValueError('The file name you\'ve entered is not valid. ' +
+                         'Please check ' +
+                         'https://info.kingcounty.gov/assessor/' +
+                         'DataDownload/default.aspx for correct file name')
 
     if num_rows is not None:
         if not isinstance(num_rows, int) & (num_rows > 0):
@@ -47,7 +70,6 @@ def get_county_data(file_name, num_rows=None):
     except OSError:
         # try three more times with delay
         for i in range(3):
-            print(i)
             time.sleep(1)
             try:
                 data_raw = pd.read_csv(url,
@@ -57,9 +79,9 @@ def get_county_data(file_name, num_rows=None):
                 pass
         if data_raw.empty:
             raise OSError('King County Assessor\'s page could not be ' +
-                          'reached. Please check ' +
+                          'reached. Please check that ' +
                           'https://info.kingcounty.gov/assessor/' +
-                          'DataDownload/default.aspx')
+                          'DataDownload/default.aspx is available')
 
     except UnicodeDecodeError:
         # change encoding to latin-1 in read_csv
