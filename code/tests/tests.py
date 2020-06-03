b512ef2c26737c9cff309cd9e033ae10fb724b4a
mweath <mweath@uw.edu>
2020-06-02 19:55:57 -0700

    adding test test

---
+++ code/tests/tests.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import unittest
+import os
+import sys
+
+class test_df(unittest.TestCase):
+    def test_king_county_data(self):
+        pass
+    def test_redfin_data(self):
+        pass
+
+if __name__ =='__main__':
+    unittest.main()
